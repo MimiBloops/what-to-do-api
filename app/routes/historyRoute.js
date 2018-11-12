62ec1e0da740f47f513ca3e28f21630ad46b7fa9
mimimario <claire.denis@viacesi.fr>
2018-11-12 22:23:01 +0100

    update History route

--- app/routes/historyRoute.js
+++ app/routes/historyRoute.js
@@ -3,27 +3,8 @@ const router = express.Router();
 
 const history = require('../controller/historyController');
 
-
-
 router.route('/')
     .get(history.list_all_history)
     .post(history.create_a_history);
 
-/*router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handeling GET request to /history'
-    });
-    history.list_all_history;
-});
-
-router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handeling post request to /history'
-    });
-    history.create_a_history;
-});
-
-/*router.route('/:isUser')
-    .get(history.list_history_of_user);
-*/
 module.exports = router;
